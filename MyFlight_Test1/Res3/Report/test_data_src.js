79e821bf1d1cf157fad267482ab1a76dacf3db4f
andreasVi <andreasvirdian07@gmail.com>
2024-02-12 16:26:36 +0700

    new 2

---
+++ MyFlight_Test1/Res3/Report/test_data_src.js
@@ -0,0 +1,1 @@
+﻿var __testDataSource={items:[{dataFilePath:'Default.xlsx',dataFileFormat:'excel',dataFileFolder:'C:\\Users\\ISGS\\Documents\\UFT One\\MyFlight\\MyFlight_Test1\\Res3',namePairs:[]}]};
